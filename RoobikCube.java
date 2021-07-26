0a14826b43c7e14b5f90e5172d10e9e21b25206d
Rhea <66195574+rjoh10@users.noreply.github.com>
2021-07-26 10:23:51 -0700

    Update RoobikCube.java

--- RoobikCube.java
+++ RoobikCube.java
@@ -1,48 +1,55 @@
 public class RoobikCube
 {
 
-    
-    private String[][] faces;
+    private RoobikFace[][] faces;
+    private char[][] outputArray;
 
-    //constructor to initialize the faces
+
+    public RoobikCube(RoobikFace[][] faces){
+        this.faces = faces;
+        this.outputArray = new char[12][12];
+    }
+
     public RoobikCube()
     {
-        this.faces = new String[][]{ { new RoobikFace("White").toString(),"","",""},
-                { new RoobikFace("Blue").toString(),
-                        new RoobikFace("Orange").toString(),
-                        new RoobikFace("Green").toString(),""},
-                { new RoobikFace("Yellow").toString(),"","",""},
-                { new RoobikFace("Red").toString(),"","",""}};
+       this.faces = new RoobikFace[][]{ {new RoobikFace(""),
+                                        new RoobikFace("White"),
+                                        new RoobikFace(""),
+                                        new RoobikFace("")},
+                { new RoobikFace("Blue"),
+                        new RoobikFace("Orange"),
+                        new RoobikFace("Green"),
+                        new RoobikFace("")},
+                {new RoobikFace(""),
+                        new RoobikFace("Yellow"),
+                        new RoobikFace(""),
+                        new RoobikFace("")},
+                {new RoobikFace(""),
+                        new RoobikFace("Red"),
+                        new RoobikFace(""),
+                        new RoobikFace("")}};
+        this.outputArray = new char[12][12];
 
     }
-    
-    //constructor to flip the faces
-    public RoobikCube(String[][] faces){
-        this.faces = faces;
-    }
 
-   //method to flip to the left
-   RoobikCube turnLeft()
-   {
-        
+    RoobikCube turnLeft()
+    {
         int rows = faces.length;
         int cols = faces[0].length;
-        String[][] rotatedLeft = new String[rows][cols];
+        RoobikFace[][] rotatedLeft = new RoobikFace[rows][cols];
         for (int i = 0; i<rows; i++){
             for (int j = 0; j<cols; j++){
                     rotatedLeft[i][j] = faces[j][cols - i - 1];
             }
         }
         return new RoobikCube(rotatedLeft);
-        
-   }
-   
-    //method to flip to the right
+    }
+
     RoobikCube turnRight()
     {
         int rows = faces.length;
         int cols = faces[0].length;
-        String[][] rotatedRight = new String[rows][cols];
+        RoobikFace[][] rotatedRight = new RoobikFace[rows][cols];
         for (int i = 0; i<rows; i++){
             for (int j = 0; j<cols; j++){
                rotatedRight[i][j] = faces[rows - j - 1][i];
@@ -51,50 +58,64 @@ public class RoobikCube
         return new RoobikCube(rotatedRight);
     }
 
-    //method to flip up
+
     RoobikCube turnUp()
     {
         int rows = faces.length;
         int cols = faces[0].length;
-        String[][] rotatedUp = new String[rows][cols];
+        RoobikFace[][] rotatedUp = new RoobikFace[rows][cols];
 
-            for (int j = 0; j<cols; j++){
-                for (int i = 0; i<rows; i++){
-                    rotatedUp[i][j] = faces[j][cols - i - 1];
+        for (int j = 0; j<cols; j++){
+            for (int i = 0; i<rows; i++){
+//                    rotatedUp[i][j] = faces[j][cols - i - 1];
+                    rotatedUp[i][j] = faces[rows-i-1][j];
             }
         }
         return new RoobikCube(rotatedUp);
     }
 
-    //method to flip down
     RoobikCube turnDown()
     {
         int rows = faces.length;
         int cols = faces[0].length;
-        String[][] rotatedDown = new String[rows][cols];
+        RoobikFace[][] rotatedDown = new RoobikFace[rows][cols];
         for (int j = 0; j<cols; j++){
             for (int i = 0; i<rows; i++){
+//                  rotatedDown[i][j] = faces[rows - 1][j];
                 rotatedDown[i][j] = faces[rows - j - 1][i];
             }
         }
         return new RoobikCube(rotatedDown);
     }
 
-    /** 
-     * @return String
-     */
     public String toString()
     {
-        int rows = faces.length;
-        int cols = faces[3].length;
-        String output = "";
-        for (int i = 0; i<rows; i++){
-            for (int j = 0; j<cols; j++){
-                output += faces[i][j];
+        RoobikFace[][] roobikFaceArray = new RoobikFace[4][4];
+        String output="";
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                roobikFaceArray[i][j] = this.faces[i][j];
+                createOutputArray(i,j,this.faces[i][j].toString().charAt(0));
             }
-            output += "\n";
+        }
+//        System.out.println(outputArray);
+        int row = outputArray.length;
+        int col = outputArray[0].length;
+        for(int i=0; i<row;i++){
+            for (int j=0; j<col; j++){
+                output +=outputArray[i][j]+" ";
+            }
+            output+= "\n";
         }
         return output;
+    }
+
+    private void createOutputArray(int rows, int cols, char faceColor){
+        for(int row=3*rows; row<3*rows+3; row++){
+            for(int col=3*cols; col<3*cols+3; col++){
+                outputArray[row][col] = faceColor;
+            }
+        }
 
     }
     
