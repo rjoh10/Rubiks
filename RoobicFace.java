c129e7218f6d9e7647a421aa9fdd15d06503f64e
Rhea <66195574+rjoh10@users.noreply.github.com>
2021-08-01 22:00:33 -0700

    Update RoobicFace.java

--- RoobicFace.java
+++ RoobicFace.java
@@ -1,31 +1,69 @@
+import java.util.Random;
+
 public class RoobikFace
 {
     private String color;
-    
-    public RoobikFace(String _color)
-    {
-        color = _color;
+    private char[][] colrArray;
+
+    enum COLOR{
+        White,
+        Blue,
+        Orange,
+        Green,
+        Yellow,
+        Red
     }
 
-    // Override
-    // Returns a 3x3 array of string for the color of the face
-    public String toString()
+    public RoobikFace(String _color)
     {
-        char[][] colrArray = new char[3][3];
-        String output="";
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if (color !=""){
-                    colrArray[i][j] = color.substring(0, 1).charAt(0);
+        this.color = _color;
+        colrArray= createRoobikFace(color);
+    }
+
+    private COLOR randomColor(){
+        int pickColor = new Random().nextInt(COLOR.values().length);
+        return COLOR.values()[pickColor];
+    }
+
+    private char[][] createRoobikFace(String color){
+        colrArray= new char[3][3];
+         for(int i=0; i<3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (color!=""){
+                    String rndmColor = randomColor().name();
+                    while(rndmColor.equalsIgnoreCase(color)){
+                        rndmColor = randomColor().name();
+                    }
+                    colrArray[i][j] = rndmColor.substring(0, 1).charAt(0);
                 }
                 else{
                     colrArray[i][j] = ' ';
                 }
+            }
+        }
+       if (color!=""){
+           colrArray[1][1] = color.substring(0, 1).charAt(0);
+       }
+        return colrArray;
+    }
 
+    public String toString()
+    {
+        String output="";
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
                 output +=colrArray[i][j]+" ";
             }
             output+= "\n";
         }
+        System.out.println(output);
         return output;
+    }
+    public static void main(String[] args){
+        RoobikFace fb = new RoobikFace("Green");
+        System.out.println(fb);
+    }
+    public char[][] getColrArray() {
+        return colrArray;
     }
 }
